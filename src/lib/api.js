0bc3aaed10bb58d76b65a4137230294ae0bc6c8c
Tero Keränen <tero-keranen@hotmail.com>
2025-09-09 21:04:41 +0300

    change base url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:4000";
-
+// const BASE_URL = "http://localhost:4000";
+const BASE_URL = process.env.BASE_URL;
 export async function apiPost(path, body, token) {
   const res = await fetch(`${BASE_URL}${path}`, {
     method: "POST",
